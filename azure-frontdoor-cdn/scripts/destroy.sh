156d66d65e4dd2ef024d690d7d884b3846dd0c8a
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-13 04:23:09 +0530

    Create destroy script for Azure Front Door CDN
    Added a script to destroy Azure Front Door CDN resources using Terraform.

--- azure-frontdoor-cdn/scripts/destroy.sh
+++ azure-frontdoor-cdn/scripts/destroy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+set -e
+
+echo "======================================"
+echo " Destroy Azure Front Door CDN"
+echo "======================================"
+
+terraform destroy -auto-approve
+
+echo ""
+echo "All Azure resources have been removed."
